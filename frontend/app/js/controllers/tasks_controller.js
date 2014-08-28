5bc68044cc7219ab98e72318451ff0b2a29739e9
Nathanael Burt <nathanael.burt@gmail.com>
2014-08-28 14:34:27 -0600

    can view individual tasks
    delete .sass-cache files and update .gitignore

--- frontend/app/js/controllers/tasks_controller.js
+++ frontend/app/js/controllers/tasks_controller.js
@@ -1,3 +1,9 @@
-angular.module("app").controller("TasksController", function ($scope, TaskResource) {
+angular.module("app").controller("TasksIndexController", function ($scope, TaskResource) {
   $scope.tasks = TaskResource.query();
+});
+
+angular.module("app").controller("TasksDetailController", function ($scope, $routeParams, $http) {
+  $http.get("/api/tasks/" + $routeParams.id).success(function (data) {
+    $scope.task = data;
+  });
 });
--- frontend/app/js/router.js
+++ frontend/app/js/router.js
@@ -1,12 +1,18 @@
-angular.module("app").config(function($routeProvider, $locationProvider) {
+angular.module("app").config(function ($routeProvider, $locationProvider) {
 
   $locationProvider.html5Mode(true);
 
-  $routeProvider.when('/tasks', {
-    templateUrl: 'index.html',
-    controller: 'TasksController'
-  });
-
-  $routeProvider.otherwise({ redirectTo: '/tasks' });
+  $routeProvider.
+    when('/tasks', {
+      templateUrl: 'index.html',
+      controller: 'TasksIndexController'
+    }).
+    otherwise({
+      redirectTo: '/tasks'
+    }).
+    when('/tasks/:id', {
+      templateUrl: 'show.html',
+      controller: 'TasksDetailController'
+    });
 
 });
