e763ccc08de5f7b61dc3d4e788ecf2a67e9a8966
Nathanael Burt <nathanael.burt@gmail.com>
2014-08-29 10:47:51 -0600

    remove rails-lineman gem

--- server/config/application.rb
+++ server/config/application.rb
@@ -26,7 +26,5 @@ module TaskManagerLineman
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    config.rails_lineman.lineman_project_location = "../client"
   end
 end
