7fbb1997ec533cc985723c298104ff39cfe2f316
Nathanael Burt <nathanael.burt@gmail.com>
2014-08-29 12:09:06 -0600

    add wildcard route to allow for page refresh

---
+++ server/app/controllers/client_controller.rb
@@ -0,0 +1,5 @@
+class ClientController < ApplicationController
+  def index
+    render "public/index.html"
+  end
+end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :tasks
   end
+
+  # rails wildcard route: not sure why we need to name the splat parameter
+  get '*_', to: 'client#index'
 end
